1e783e9586c3bf4c4e1c273c8f0a6b356e83a330
mgd <mr.zizikk@gamil.com>
2023-07-16 21:03:05 -0400

    create config class with db variables

---
+++ src/config/config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+
+__all__ = (
+    'settings',
+)
+
+load_dotenv()
+
+
+class BaseConfig:
+    DB_USER = os.environ.get('DB_USER', 'postgres')
+    DB_NAME = os.getenv('DB_NAME', 'postgres')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '5432')
+
+    DATABASE_URL = f'postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+
+settings = BaseConfig()
