e5c39d3cbe3ad5af14c131d7fbf247f5d1cf3caa
mgd <mr.zizikk@gamil.com>
2023-07-16 21:24:43 -0400

    add docstring to schemas

--- src/schemas.py
+++ src/schemas.py
@@ -11,23 +11,28 @@ RatePydantic = pydantic_model_creator(Rate)
 
 
 class InsuranceResponse(BaseModel):
+    """Insurance response schema."""
     insurance_coast: float
 
 
 class RateCreatePydantic(BaseModel):
+    """New rate create schema."""
     cargo_type: str
     date: datetime.date
     rate: decimal.Decimal
 
 
 class RateListPydantic(BaseModel):
+    """List fo rates schema."""
     rates: list[RatePydantic]
 
 
 class RateRequest(BaseModel):
+    """Schema for file data serializing."""
     cargo_type: str
     rate: decimal.Decimal
 
 
-class RateRequestModel(BaseModel):
+class RateFileRequest(BaseModel):
+    """Schema for upload data from file."""
     _: dict[datetime.date, list[RateRequest]]
