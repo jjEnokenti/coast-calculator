fe7599fa393eefd1daab701d48bdad6a61e79655
mgd <mr.zizikk@gamil.com>
2023-07-16 21:24:15 -0400

    add docstring to endpoints

--- src/endpoints.py
+++ src/endpoints.py
@@ -24,13 +24,15 @@ app = FastAPI()
 
 
 @app.get('/calculate_insurance/',
+         description='Get calculate of insurance',
          response_model=InsuranceResponse,
          status_code=status.HTTP_200_OK)
-async def get_rate(date: datetime.date, price: int, cargo_type: str):
+async def get_calculate_insurance(date: datetime.date, price: int, cargo_type: str):
     return await calculate_insurance(date=date, price=price, cargo_type=cargo_type)
 
 
 @app.get('/rates/',
+         description='Get list of rates',
          response_model=RateListPydantic,
          status_code=status.HTTP_200_OK)
 async def get_list_of_rate():
@@ -38,6 +40,7 @@ async def get_list_of_rate():
 
 
 @app.post('/upload_file_tariff/',
+          description='Upload data from json file',
           response_model=RateListPydantic,
           status_code=status.HTTP_201_CREATED)
 async def upload_file_with_rates(data: UploadFile = File(...)):
@@ -45,6 +48,7 @@ async def upload_file_with_rates(data: UploadFile = File(...)):
 
 
 @app.post('/upload_tariff/',
+          description='Upload data from json string',
           response_model=RateListPydantic,
           status_code=status.HTTP_201_CREATED)
 async def load_rates(data: list[RateCreatePydantic]):
